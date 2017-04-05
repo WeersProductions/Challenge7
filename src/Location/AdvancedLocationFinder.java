4eeab025aa2f7338c3ab62e70776c83f1aebf4ce
WeersProductions <weersproductions@gmail.com>
2017-04-05 12:18:46 +0200

    Add better radius

--- src/Location/AdvancedLocationFinder.java
+++ src/Location/AdvancedLocationFinder.java
@@ -16,8 +16,8 @@ import java.util.*;
  */
 public class AdvancedLocationFinder implements LocationFinder{
 
-    private final int A = -46;
-    private final float N = -2.7f;
+    private final int A = -30;
+    private final float N =2f;
 
 	private HashMap<String, Position> knownLocations; //Contains the known locations of APs. The long is a MAC address.
 
@@ -44,18 +44,11 @@ public class AdvancedLocationFinder implements LocationFinder{
 
 		//Where the value is the MilliWatt.
 		Map<MacRssiPair, Double> macRssiPairMap= new HashMap<>();
-//		MacRssiPair closest = null;
-//		double closestDistance = 0;
         //Create a map with only the known locations.
         for (MacRssiPair pair : data) {
             if(knownLocations.containsKey(pair.getMacAsString())) {
                 double milliWat = dbmToMilliWatt(pair.getRssi());
                 macRssiPairMap.put(pair, milliWat);
-//
-//                if(closestDistance < milliWat) {
-//                    closest = pair;
-//                    closestDistance = milliWat;
-//                }
             }
         }
 
@@ -63,24 +56,47 @@ public class AdvancedLocationFinder implements LocationFinder{
         Map<MacRssiPair, Double> macRssiPairDistance = new HashMap<>();
         MacRssiPair closestPair = null;
         for(Map.Entry<MacRssiPair, Double> entry : macRssiPairMap.entrySet()) {
-            macRssiPairDistance.put(entry.getKey(), Math.pow(10, (entry.getKey().getRssi() - A) / (-10 * N)));
-            if(macRssiPairDistance.get(entry.getKey()) < (closestPair == null ? Integer.MAX_VALUE : macRssiPairDistance.get(closestPair))) {
+			macRssiPairDistance.put(entry.getKey(), Math.pow(10, (entry.getKey().getRssi() - A) / (-10 * N)));
+			System.out.println(knownLocations.get(entry.getKey().getMacAsString()) + "    " +macRssiPairDistance.get(entry.getKey()));
+			if(macRssiPairDistance.get(entry.getKey()) < (closestPair == null ? Integer.MAX_VALUE : macRssiPairDistance.get(closestPair))) {
                 closestPair = entry.getKey();
-            }
+			}
         }
 
         //We shift the closest one and set the position to 0,0.
-        Position shift = new Position(-knownLocations.get(closestPair).getX(), -knownLocations.get(closestPair).getY());
+		Position shift = new Position(-knownLocations.get(closestPair.getMacAsString()).getX(), -knownLocations.get(closestPair.getMacAsString()).getY());
 
-        //A map with the shifted position.
+		//A map with the shifted position.
         Map<MacRssiPair, Position> macRssiPairShiftedPosition = new HashMap<>();
 
         //Shift all positions.
         for(Map.Entry<MacRssiPair, Double> entry : macRssiPairDistance.entrySet()) {
-            macRssiPairShiftedPosition.put(entry.getKey(), new Position(knownLocations.get(entry.getKey()).getX() + shift.getX(), knownLocations.get(entry.getKey()).getY() + shift.getY()));
-        }
+            macRssiPairShiftedPosition.put(entry.getKey(), new Position(knownLocations.get(entry.getKey().getMacAsString()).getX() + shift.getX(), knownLocations.get(entry.getKey().getMacAsString()).getY() + shift.getY()));
+		}
 
-        
+        double r1 = macRssiPairDistance.get(closestPair);
+        double r2 = 0;
+		for (Map.Entry<MacRssiPair, Position> entry : macRssiPairShiftedPosition.entrySet()) {
+			if(entry.getValue().getX() == 0 ^ entry.getValue().getY() == 0) {
+				r2 = macRssiPairDistance.get(entry.getKey());
+				break;
+			}
+		}
+
+		double r3 = 0;
+		for(Map.Entry<MacRssiPair, Position> entry : macRssiPairShiftedPosition.entrySet()) {
+			if(entry.getValue().getX() != 0 && entry.getValue().getY() != 0) {
+				r3 = macRssiPairDistance.get(entry.getKey());
+				break;
+			}
+		}
+
+		System.out.println("r1: " + r1);
+		System.out.println("r2: " + r2);
+		System.out.println("r3: " + r3);
+		System.out.println("---------------");
+
+
 
 //        //Where the value is the relative distance.
 //        Map<MacRssiPair, Double> macRssiPairRelativeDistance = new HashMap<>();
@@ -93,7 +109,7 @@ public class AdvancedLocationFinder implements LocationFinder{
 //                macRssiPairRelativeDistance.put(entry.getKey(), Math.pow(2, Math.log(closestDistance) - Math.log(entry.getValue())));
 //        }
 
-		System.out.println("--------");
+		//System.out.println("--------");
 		return getFirstKnownFromList(data); //return the first known APs location
 	}
 	
